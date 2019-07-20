2589b581af6c9020b5a5c1b0f68eb02fe69cc71e
Ronaldo Givisiez <ronaldogivisiez@gmail.com>
2019-07-20 18:01:01 -0300

    Create Poster.py

---
+++ Poster.py
@@ -0,0 +1,23 @@
+# I used a figure size fitted for a thesis. It worked well, but in future I will change this parameters.
+
+plt.rcParams['figure.figsize'] = [6.32, 5.11]
+plt.rcParams['figure.constrained_layout.use'] = True
+
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['axes.linewidth'] = 3
+plt.rcParams['axes.labelpad'] = 15
+
+plt.rcParams['xtick.labelsize'] = 16
+plt.rcParams['xtick.major.size'] = 8
+plt.rcParams['xtick.major.width'] = 3
+
+plt.rcParams['ytick.labelsize'] = 16
+plt.rcParams['ytick.major.size'] = 8
+plt.rcParams['ytick.major.width'] = 3
+
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['legend.markerscale'] = 1
+
+plt.rcParams['lines.markersize'] = 9
+plt.rcParams['lines.linewidth'] = 2
